bc3f8bd0bd08ae8a44cbb62f0af00a7fb1c74a23
luke lincoln <luke@canva.com>
2021-11-22 00:16:39 +1100

    setup

--- index.js
+++ index.js
@@ -1,4 +1,20 @@
 async function main() {
-    
+    const wasmFile = await fetch('./snake.wasm').then(r => r.arrayBuffer());
+
+    const memory = new WebAssembly.Memory({ initial: 1 });
+    const boardWidth = new WebAssembly.Global({value:'i32', mutable:true}, 25);
+    const boardHeight = new WebAssembly.Global({value:'i32', mutable:true}, 25);
+    const { instance } = await WebAssembly.instantiate(wasmFile, {
+        env: {
+            memory,
+            boardWidth,
+            boardHeight,
+            pixelOut: (x, y, pixelType) => {
+                console.log(x, y, pixelType);
+            }
+        },
+    });
+    console.log(instance);
+    console.log(instance.exports.add(5, 6));
 }
 main();
--- snake.wat
+++ snake.wat
@@ -1,6 +1,18 @@
 (module
+    ;; memory
+    (import "env" "memory" (memory 1))
+    ;; board width & height
+    (global $boardWidth (import "env" "boardWidth") (mut i32))
+    (global $boardHeight (import "env" "boardHeight") (mut i32))
+    ;; system functionality
+    ;; eg pixelOut(x, y, 2 | 1 | 0) (2=cherry, 1=snake, 0=blank)
+    (import "env" "pixelOut" (func $pixelOut (param i32) (param i32) (param i32)))
     (export "add" (func $add))
     (func $add (param $a i32) (param $b i32) (result i32)
+        i32.const 0
+        i32.const 2
+        i32.const 1
+        call $pixelOut
         get_local $a
         get_local $b
         i32.add
